3afef03e1a2a9cdab60acf99ffbbb6b0bb960eea
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-11-16 21:25:48 +0700

    done 37

--- src/v_builders/_37_StringAndMapBuilders.kt
+++ src/v_builders/_37_StringAndMapBuilders.kt
@@ -27,13 +27,18 @@ fun todoTask37(): Nothing = TODO(
         building it and returning it as a result.
     """
 )
+fun buildMap(build: MutableMap<Int,String>.() -> Unit): Map<Int, String> {
+    var map = HashMap<Int,String>()
+    map.build()
+    return map
+}
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    //todoTask37()
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
